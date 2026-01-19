772158d4acdb80f573a70e720056ce3403c644a3
hariprathap <hariprathap@conversedatasolutions.com>
2026-01-19 20:23:36 +0530

    completed day 01 '19-01-2026'

---
+++ day-01/script.js
@@ -0,0 +1,5 @@
+console.log("i am from external file called script.js")
+
+console.log(document.getElementById('emptyDiv'))
+console.log(document.getElementById('emptyDiv1'))
+document.getElementById('emptyDiv1').innerText('Hi theree')
