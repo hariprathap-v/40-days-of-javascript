ffb6504c080c37ee8669c78d84ebb4e4fdda161c
Hariprathap-converse <hariprathap@conversedatasolutions.com>
2026-03-12 10:24:03 +0530

    day 02 task completed

---
+++ day-02/script.js
@@ -0,0 +1,32 @@
+console.log("day 02");
+
+let person = "Hari";
+let age = 22;
+let isStudent = true;
+let favoriteLanguage = "JavaScript";
+const phoneNumber = 1234567890;
+console.log("person: ", person);
+console.log("age: ", age);
+console.log("isStudent: ", isStudent);
+console.log("favoriteLanguage: ", favoriteLanguage);
+console.log("*******************************************");
+person = "prathap";
+age = 23;
+isStudent = false;
+console.log("after update", person);
+console.log("after update", age);
+console.log("after update", isStudent);
+console.log("after update", favoriteLanguage);
+console.log("*******************************************");
+console.log("phoneNumber", phoneNumber);
+// phoneNumber = 10987654321;
+// console.log("phoneNumber", phoneNumber);
+
+let personData = {
+  name: "hari",
+  age: 22,
+  isStudent: true,
+  favoriteLanguages: ["HTML", "Css", "JavaScript"],
+};
+console.log("personData", personData);
+console.log("person is favoriteLanguages", personData.favoriteLanguages);
